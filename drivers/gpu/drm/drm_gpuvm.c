9c7231768d6a128c1fd07ec18ab5ce3b8ad3bced
Caterina Shablia <caterina.shablia@collabora.com>
2025-09-02 17:33:38 +0000

    drm/gpuvm: Add a flags field to drm_gpuva_op_map
    drm_gpuva objects have a flags field. Currently, this can be managed by
    drivers out-of-band, without any special handling in drm_gpuvm.
    
    To be able to introduce flags that do affect the logic in the drm_gpuvm
    core, we need to plumb it through the map calls. This will allow the
    core to check the flags on map and alter the merge/split logic depending
    on the requested flags and the flags of the existing drm_gpuva ranges
    that are being split.
    
    Signed-off-by: Asahi Lina <lina+kernel@asahilina.net>
    Signed-off-by: Caterina Shablia <caterina.shablia@collabora.com>

--- drivers/gpu/drm/drm_gpuvm.c
+++ drivers/gpu/drm/drm_gpuvm.c
@@ -2407,6 +2407,14 @@ op_unmap_cb(const struct drm_gpuvm_ops *fn, void *priv,
 	return fn->sm_step_unmap(&op, priv);
 }
 
+static bool can_merge_flags(struct drm_gpuvm *gpuvm, enum drm_gpuva_flags a,
+			    enum drm_gpuva_flags b)
+{
+	if (gpuvm->ops->sm_can_merge_flags)
+		return gpuvm->ops->sm_can_merge_flags(a, b);
+	return a == b;
+}
+
 static bool __can_merge(struct drm_gpuvm *gpuvm, const struct drm_gpuva_op_map *a,
 			const struct drm_gpuva_op_map *b)
 {
@@ -2414,6 +2422,9 @@ static bool __can_merge(struct drm_gpuvm *gpuvm, const struct drm_gpuva_op_map *
 	 * the same GEM object.
 	 */
 	if (a->gem.obj != b->gem.obj || !a->gem.obj)
+		return false;
+
+	if (can_merge_flags(gpuvm, a->flags, b->flags))
 		return false;
 
 	/* Order VAs for the rest of the checks. */
@@ -2440,6 +2451,7 @@ static bool can_merge(struct drm_gpuvm *gpuvm, const struct drm_gpuva *a,
 		.va.range = a->va.range,
 		.gem.offset = a->gem.offset,
 		.gem.obj = a->gem.obj,
+		.flags = a->flags,
 	};
 
 	return __can_merge(gpuvm, &tmp, b);
@@ -2498,6 +2510,7 @@ __drm_gpuvm_sm_map(struct drm_gpuvm *gpuvm,
 					.va.range = range - req_range,
 					.gem.obj = obj,
 					.gem.offset = offset + req_range,
+					.flags = va->flags,
 				};
 				struct drm_gpuva_op_unmap u = {
 					.va = va,
@@ -2519,6 +2532,7 @@ __drm_gpuvm_sm_map(struct drm_gpuvm *gpuvm,
 				.va.range = ls_range,
 				.gem.obj = obj,
 				.gem.offset = offset,
+				.flags = va->flags,
 			};
 			struct drm_gpuva_op_unmap u = { .va = va };
 
@@ -2562,6 +2576,7 @@ __drm_gpuvm_sm_map(struct drm_gpuvm *gpuvm,
 					.gem.obj = obj,
 					.gem.offset = offset + ls_range +
 						      req_range,
+					.flags = va->flags,
 				};
 
 				ret = op_remap_cb(ops, priv, &p, &n, &u);
@@ -2599,6 +2614,7 @@ __drm_gpuvm_sm_map(struct drm_gpuvm *gpuvm,
 					.va.range = end - req_end,
 					.gem.obj = obj,
 					.gem.offset = offset + req_end - addr,
+					.flags = va->flags,
 				};
 				struct drm_gpuva_op_unmap u = {
 					.va = va,
@@ -2650,6 +2666,7 @@ __drm_gpuvm_sm_unmap(struct drm_gpuvm *gpuvm,
 			prev.va.range = req_addr - addr;
 			prev.gem.obj = obj;
 			prev.gem.offset = offset;
+			prev.flags = va->flags;
 
 			prev_split = true;
 		}
@@ -2659,6 +2676,7 @@ __drm_gpuvm_sm_unmap(struct drm_gpuvm *gpuvm,
 			next.va.range = end - req_end;
 			next.gem.obj = obj;
 			next.gem.offset = offset + (req_end - addr);
+			next.flags = va->flags;
 
 			next_split = true;
 		}
